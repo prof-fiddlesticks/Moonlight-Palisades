f9a29b1641e82bdf445b0f9b28cd2c1c226cd751
prof-fiddlesticks <ishaandokania1@gmail.com>
2026-02-10 19:16:30 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,23 @@
+package main;
+
+import javax.swing.JFrame;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame window = new JFrame("Moonlight Palisades");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("Moonlight Palisades");
+
+        GamePanel gamePanel = new GamePanel ();
+        window.add(gamePanel);
+
+        window.pack();
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+        };
+    }
+
 
