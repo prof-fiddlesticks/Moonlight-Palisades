3c14de3aabc1ba46854492c0c7e843df9eae4673
prof-fiddlesticks <ishaandokania1@gmail.com>
2026-02-10 19:16:55 -0800

    Update GamePanel.java

--- GamePanel.java
+++ GamePanel.java
@@ -1,1 +1,22 @@
+package main;
+
+import javax.swing.JPanel;
+import java.awt.Color;
+import java.awt.Dimension;
+
+public class GamePanel extends JPanel{
+    final int originalTileSize = 16;
+    final int scale = 3;
+    final int tileSize = originalTileSize * scale;
+    final int maxScreenCol = 16;
+    final int maxScreenRow = 12;
+    final int screenWidth = tileSize * maxScreenCol;
+    final int screenHeight = tileSize * maxScreenRow;
+    public GamePanel() {
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(Color.black);
+        this.setDoubleBuffered(true);
+
+    }
+}
 
